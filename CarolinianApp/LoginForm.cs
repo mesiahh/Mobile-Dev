2d0431ae79b8e75f223018ff95295cca9d7dc930
kel <144028220+mesiahh@users.noreply.github.com>
2025-03-05 20:55:27 +0800

    Add files via upload

--- CarolinianApp/LoginForm.cs
+++ CarolinianApp/LoginForm.cs
@@ -9,56 +9,149 @@ namespace CarolinianApp
         public LoginForm()
         {
             this.Text = "Login";
-            this.Size = new Size(350, 500);
+            this.Size = new Size(400, 600);
             this.FormBorderStyle = FormBorderStyle.FixedDialog;
             this.MaximizeBox = false;
-            
+            this.BackColor = Color.White;
+
+            // Adding logo image
+            PictureBox logo = new PictureBox()
+            {
+                Image = Image.FromFile("carolin.jfif"),
+                SizeMode = PictureBoxSizeMode.StretchImage,
+                Size = new Size(120, 120),
+                Location = new Point(140, 20)
+            };
+            this.Controls.Add(logo);
+
             Label lblLogin = new Label()
             {
                 Text = "Login",
-                Font = new Font("Arial", 14, FontStyle.Bold),
+                Font = new Font("Montserrat", 16, FontStyle.Bold),
                 AutoSize = true,
-                Location = new Point(140, 100)
+                Location = new Point(165, 150)
             };
             this.Controls.Add(lblLogin);
-            
-            TextBox txtEmail = new TextBox() { PlaceholderText = "Email", Location = new Point(75, 140), Width = 200 };
-            TextBox txtPassword = new TextBox() { PlaceholderText = "Password", Location = new Point(75, 180), Width = 200, PasswordChar = '*' };
+
+            // Standard TextBoxes with Placeholder Behavior
+            TextBox txtEmail = CreatePlaceholderTextBox("Email", new Point(37, 190));
+            TextBox txtPassword = CreatePlaceholderTextBox("Password", new Point(37, 259), true);
             this.Controls.Add(txtEmail);
             this.Controls.Add(txtPassword);
-            
+
+            // Forgot Password
             Label lblForgotPassword = new Label()
             {
                 Text = "Forgot Password?",
-                ForeColor = Color.FromArgb(0x5F, 0x5F, 0x5F), // Color #5F5F5F
-                Font = new Font("Arial", 9, FontStyle.Underline),
+                ForeColor = Color.Blue,
+                Font = new Font("Montserrat", 10, FontStyle.Underline),
                 AutoSize = true,
-                Location = new Point(75, 210),
-                Cursor = Cursors.Hand // Makes it look clickable
+                Location = new Point(240, 290),
+                Cursor = Cursors.Hand
             };
-            lblForgotPassword.Click += (sender, e) => { new FindAccountForm().ShowDialog(); };
+            lblForgotPassword.Click += (sender, e) =>
+            {
+                this.Hide(); // Hide login form
+                new FindAccountForm().ShowDialog(); // Open forgot password form
+                this.Show(); // Show login form again after closing FindAccountForm
+            };
             this.Controls.Add(lblForgotPassword);
+            lblForgotPassword.BringToFront(); // Ensure visibility
 
+            // Login Button
             Button btnLogin = new Button()
             {
                 Text = "Login",
-                Location = new Point(125, 240),
-                Width = 100,
-                BackColor = Color.FromArgb(0xF2, 0x72, 0x2B), // Color #F2722B
-                ForeColor = Color.White
+                Location = new Point(37, 320),
+                Width = 326,
+                Height = 56,
+                BackColor = Color.FromArgb(242, 114, 43),
+                ForeColor = Color.White,
+                FlatStyle = FlatStyle.Flat
             };
+            btnLogin.FlatAppearance.BorderSize = 0;
             this.Controls.Add(btnLogin);
 
-            Button btnSignUp = new Button()
+            // OR Divider
+            Label lblOr = new Label()
             {
-                Text = "Sign Up",
-                Location = new Point(125, 280),
-                Width = 100,
-                BackColor = Color.Gray,
-                ForeColor = Color.White
+                Text = "OR",
+                ForeColor = Color.Gray,
+                Font = new Font("Montserrat", 10, FontStyle.Bold),
+                AutoSize = true,
+                Location = new Point(180, 390)
             };
-            btnSignUp.Click += (sender, e) => { this.Hide(); new SignUpForm().ShowDialog(); this.Show(); };
-            this.Controls.Add(btnSignUp);
+            this.Controls.Add(lblOr);
+
+            // Social Login Buttons
+            this.Controls.Add(CreateSocialIcon("facebook.png", new Point(90, 420)));
+            this.Controls.Add(CreateSocialIcon("google.png", new Point(175, 420)));
+            this.Controls.Add(CreateSocialIcon("x.png", new Point(260, 420)));
+
+            // Sign-up Label (Navigates to SignUpForm)
+            Label lblSignUp = new Label()
+            {
+                Text = "Don't have an Account? Sign Up",
+                ForeColor = Color.FromArgb(242, 114, 43),
+                Font = new Font("Montserrat", 9, FontStyle.Underline),
+                AutoSize = true,
+                Location = new Point(100, 480),
+                Cursor = Cursors.Hand
+            };
+            lblSignUp.Click += (sender, e) => 
+            {
+                this.Hide();
+                new SignUpForm().ShowDialog();
+                this.Show();
+            };
+            this.Controls.Add(lblSignUp);
+        }
+
+        // Helper method for creating TextBoxes with placeholder behavior
+        private TextBox CreatePlaceholderTextBox(string placeholder, Point location, bool isPassword = false)
+        {
+            TextBox textBox = new TextBox()
+            {
+                Text = placeholder,
+                ForeColor = Color.Gray,
+                Font = new Font("Montserrat", 10),
+                Width = 326,
+                Location = location
+            };
+
+            textBox.Enter += (s, e) =>
+            {
+                if (textBox.Text == placeholder)
+                {
+                    textBox.Text = "";
+                    textBox.ForeColor = Color.Black;
+                    if (isPassword) textBox.UseSystemPasswordChar = true;
+                }
+            };
+
+            textBox.Leave += (s, e) =>
+            {
+                if (string.IsNullOrWhiteSpace(textBox.Text))
+                {
+                    textBox.Text = placeholder;
+                    textBox.ForeColor = Color.Gray;
+                    if (isPassword) textBox.UseSystemPasswordChar = false;
+                }
+            };
+
+            return textBox;
+        }
+
+        // Helper method for creating social media icons
+        private PictureBox CreateSocialIcon(string imagePath, Point location)
+        {
+            return new PictureBox()
+            {
+                Image = Image.FromFile(imagePath),
+                SizeMode = PictureBoxSizeMode.StretchImage,
+                Size = new Size(40, 40),
+                Location = location
+            };
         }
     }
 }
--- CarolinianApp/SignupForm.cs
+++ CarolinianApp/SignupForm.cs
@@ -4,7 +4,7 @@ using System.Drawing;
 
 namespace CarolinianApp
 {
-    public class SignUpForm : Form
+   public class SignUpForm : Form
     {
         public SignUpForm()
         {
@@ -12,27 +12,37 @@ namespace CarolinianApp
             this.Size = new Size(350, 500);
             this.FormBorderStyle = FormBorderStyle.FixedDialog;
             this.MaximizeBox = false;
-            
+
+            // Adding logo image
+            PictureBox logo = new PictureBox()
+            {
+                Image = Image.FromFile("carolin.jfif"), // Ensure the image exists in the directory
+                SizeMode = PictureBoxSizeMode.StretchImage,
+                Size = new Size(100, 100),
+                Location = new Point(125, 20)
+            };
+            this.Controls.Add(logo);
+
             Label lblSignUp = new Label()
             {
-                Text = "Sign In",
+                Text = "Sign Up",
                 Font = new Font("Arial", 14, FontStyle.Bold),
                 AutoSize = true,
-                Location = new Point(140, 80)
+                Location = new Point(140, 130)
             };
             this.Controls.Add(lblSignUp);
-            
-            TextBox txtUsername = new TextBox() { PlaceholderText = "Username", Location = new Point(75, 120), Width = 200 };
-            TextBox txtEmail = new TextBox() { PlaceholderText = "Email", Location = new Point(75, 160), Width = 200 };
-            TextBox txtPassword = new TextBox() { PlaceholderText = "Password", Location = new Point(75, 200), Width = 200, PasswordChar = '*' };
-            TextBox txtConfirmPassword = new TextBox() { PlaceholderText = "Confirm Password", Location = new Point(75, 240), Width = 200, PasswordChar = '*' };
+
+            TextBox txtUsername = new TextBox() { PlaceholderText = "Username", Location = new Point(75, 160), Width = 200 };
+            TextBox txtEmail = new TextBox() { PlaceholderText = "Email", Location = new Point(75, 200), Width = 200 };
+            TextBox txtPassword = new TextBox() { PlaceholderText = "Password", Location = new Point(75, 240), Width = 200, PasswordChar = '*' };
+            TextBox txtConfirmPassword = new TextBox() { PlaceholderText = "Confirm Password", Location = new Point(75, 280), Width = 200, PasswordChar = '*' };
             
             this.Controls.Add(txtUsername);
             this.Controls.Add(txtEmail);
             this.Controls.Add(txtPassword);
             this.Controls.Add(txtConfirmPassword);
-            
-            Button btnSignUp = new Button() { Text = "Sign Up", Location = new Point(125, 280), Width = 100, BackColor = Color.Orange, ForeColor = Color.White };
+
+            Button btnSignUp = new Button() { Text = "Sign Up", Location = new Point(125, 320), Width = 100, BackColor = Color.Orange, ForeColor = Color.White };
             btnSignUp.Click += (sender, e) => { this.Hide(); new LoginForm().ShowDialog(); this.Show(); };
             this.Controls.Add(btnSignUp);
         }
